991892867f9bda4c486bca4e98b90e907cb33f0d
Patrice Johnson <pjtheupgrade@gmail.com>
2025-06-23 10:24:23 -0400

    Set Home page as default route

--- src/App.js
+++ src/App.js
@@ -1,21 +1,2 @@
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <iframe
-        src="https://docs.google.com/forms/d/e/1FAIpQLSeqVmSjPD1q1vOj_E8uVROfvE_jWZCV9cmW1nzEuAZkF3Hy8w/viewform?embedded=true"
-        width="100%"
-        height="1200"
-        frameBorder="0"
-        marginHeight="0"
-        marginWidth="0"
-        title="Client Intake Form"
-      >
-        Loading…
-      </iframe>
-    </div>
-  );
-}
-
-export default App;
+<Route path="/" element={<Home />} />
+<Route path="/intake" element={<Intake />} />
