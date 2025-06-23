96354e7611ec40977af02bbf758609edf10bd7b2
Patrice Johnson <pjtheupgrade@gmail.com>
2025-06-23 13:03:21 -0400

    Set Home as default landing page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-<Routes>
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Intake from './pages/Intake';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
   <Route path="/" element={<Home />} />
   <Route path="/intake" element={<Intake />} />
 </Routes>
-import React from 'react';<nav className="flex justify-between items-center py-4">
-  <a href="/" className="font-bold text-lg text-indigo-900">Wellfitebooks</a>
-  <a href="/intake" className="text-emerald-600 hover:underline">Start SEO Intake</a>
-</nav>
+
+    </Router>
+  );
+}
